85efed7448bd5acfaf50bb3df51ae60b1ad47b6e
Dana Rocha <dana.rocha@bigbank.ee>
2020-05-06 22:25:00 +0300

    feat: make add-ons available for storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,20 @@
 import { configure, addParameters, addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import centered from '@storybook/addon-centered/react';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 import theme from './theme';
 
 import './reset.css';
 
 addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+  backgrounds: [
+    { name: 'dark', value: '#e3e3e3', default: true },
+    { name: 'light', value: '#6786' },
+  ],
   options: {
     theme,
   },
--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  addons: [
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-knobs/register',
+  ],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
