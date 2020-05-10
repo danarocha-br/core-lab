c4b3d7114d5ed4d332e262b8633fb44aeca0165d
danarocha <dana@danarocha.com.br>
2020-05-10 13:33:51 +0300

    feat: make initial theme config

--- src/styles/themes/light.ts
+++
@@ -1,6 +0,0 @@
-// .theme-light {
-//   --bg-background-primary: #453eeb;
-//   --bg-background-secondary: #8c9cba;
-//   --bg-background-ternary: #fbfcff;
-//   --bg-background-shade: #e4eaf6;
-// }
